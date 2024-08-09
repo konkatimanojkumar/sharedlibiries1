70596ff7af7d53e9b4609d087cf59390e8319a63
konkatimanojkumar <konkatimanojkumar4@gmail.com>
2024-08-09 12:50:27 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,6 +8,6 @@ def mavenbuild()
 }
 def gitdeploy(jobname,ipaddress,context)
 {
-   sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}/var/lib/tomcat9/webapps/${context}.war"
+   sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
